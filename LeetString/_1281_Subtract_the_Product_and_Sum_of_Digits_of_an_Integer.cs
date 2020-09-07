b34310ef4b0cb170402cc48f0186ba359b62b142
im2f1 <judge0648@gmail.com>
2020-09-07 15:55:13 +0800

    1281. Subtract the Product and Sum of Digits of an Integer

---
+++ LeetString/_1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetString
+{
+    public class _1281_Subtract_the_Product_and_Sum_of_Digits_of_an_Integer
+    {
+        public int SubtractProductAndSum(int n)
+        {
+            var div = n;
+            var str = n.ToString();
+            var multiply = 1;
+            var plus = 0;
+            while (div >= 1)
+            {
+                var tmp = div % 10;
+                multiply *= tmp;
+                plus += tmp;
+                div = div / 10;
+            }
+
+            return (multiply - plus);
+
+            //foreach (var s in str)
+            //{
+                
+            //}
+            //return 15;
+        }
+    }
+}
--- LeetStringTests/_1108DefangAnIpAddressTests.cs
+++ LeetStringTests/_1108DefangAnIpAddressTests.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LeetString;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LeetString.Tests
 {
