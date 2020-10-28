1be4c350b2686b5f6f23f7ffb125e04dd72aa52e
im2f1 <judge0648@gmail.com>
2020-10-28 20:47:00 +0800

    1480. Running Sum of 1d Array

---
+++ LeetStringFramework/_1480_Running_Sum_of_1d_Array.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetStringFramework
+{
+    public class _1480_Running_Sum_of_1d_Array
+    {
+        public int[] RunningSum(int[] nums)
+        {
+
+            var sum = 0;
+            var ret = new List<int>();
+            foreach (var num in nums)
+            {
+                sum += num;
+                ret.Add(sum);
+            }
+
+            return ret.ToArray();
+        }
+        //var ret = new int[nums.Length];
+        //for (int i = 0; i < nums.Length; i++)
+        //{
+        //    ret[i] = sum + nums[i];
+        //    sum = ret[i];
+        //}
+    }
+}
---
+++ LeetStringFrameworkTests/_1480_Running_Sum_of_1d_ArrayTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetStringFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetStringFramework.Tests
+{
+    [TestClass()]
+    public class _1480_Running_Sum_of_1d_ArrayTests
+    {
+        [TestMethod()]
+        public void RunningSumTest()
+        {
+            var _1480= new _1480_Running_Sum_of_1d_Array();
+            var nums = new int[] { 1, 2, 3, 4 };
+            var expect = new int[] {1, 3, 6, 10};
+            var ret = _1480.RunningSum(nums);
+            CollectionAssert.AreEqual(expect,ret);
+        }
+    }
+}
